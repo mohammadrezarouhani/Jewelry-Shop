a6a7425e37ddd752e108eedf0ce6f5c0869b21d2
mohammadreza rouhani <mohammadrezarouhani@protonmail.com>
2022-04-30 14:01:52 +0430

    password-change added

--- core/user/url.py
+++ core/user/url.py
@@ -1,8 +1,8 @@
-from django.urls import path,include
-from rest_framework.routers import DefaultRouter
-from .views import UserRegister,UserDetail
+from django.urls import path
+from .views import UserRegister,UserDetail,PasswordReset
 
 urlpatterns=[
     path('register/',UserRegister.as_view(),name='uer-register'),
     path('detail/<str:pk>/',UserDetail.as_view(),name='user-detail'),
+    path('password-change/<str:pk>/',PasswordReset.as_view(),name='password-forget'),
 ]
